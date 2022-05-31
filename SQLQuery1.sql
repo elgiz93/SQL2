2687e18c6e3105f1e590f8fbffd059192a4682d2
Elgiz <98798356+elgiz93@users.noreply.github.com>
2022-05-31 09:20:04 +0400

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,7 +4,7 @@ use company
 
 create table Departments
 (
-Id int primary key,
+Id int primary key identity,
 Name nvarchar(25) not null check (len(Name)>2)
 )
 
@@ -12,7 +12,7 @@ Name nvarchar(25) not null check (len(Name)>2)
 
 create table Employees
 (
-Id int,
+Id int primary key identity,
 FullName nvarchar(25) not null check(len(FullName)>3),
 Salary int check (Salary>0),
 Email nvarchar(25) not null unique,
